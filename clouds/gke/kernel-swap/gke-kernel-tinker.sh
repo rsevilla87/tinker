e161062313ad178b413f6f179bdc7df1d0ed319a
Joe Talerico <rook@isovalent.com>
2022-02-01 15:13:11 -0500

    Initial tinker
    First cloud tinker. Swap them kernels for me!

---
+++ clouds/gke/kernel-swap/gke-kernel-tinker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -ex
+region=us-central1-a
+name="${USER}-tinker"
+gcloud container clusters create "$name" \
+    --labels "usage=tinker,owner=${USER}" \
+    --num-nodes 2 \
+    --node-taints node.cilium.io/agent-not-ready=true:NoSchedule \
+    --image-type=UBUNTU_CONTAINERD \
+    --region $region 
+cilium install
+kubectl create -f cm-kernel.yaml
+envsubst < ds-kernel.yaml
+export KERNEL_VERSION=5.16
+envsubst < ds-kernel.yaml | kubectl apply -f -
+kubectl rollout status ds/node-initializer
+for node in $(kubectl get nodes -o=custom-columns=:.metadata.name | grep -v -e '^[[:space:]]*$'); do
+    gcloud compute instances reset --zone $region $node
+done
+sleep 10 # Wait for k8s to catchup to the reboot 
+kubectl wait --for=condition=Ready nodes --all --timeout=600s 
