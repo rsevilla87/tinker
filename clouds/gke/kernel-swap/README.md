827d3f92fed4e9636aabdcf6b5149cdf0288b566
Joe Talerico <rook@isovalent.com>
2022-02-03 08:17:51 -0500

    Update README and allow to run w/o creating
    - Mention the Caveat of updating the kernel in GKE
    - Allow script to run w/o actually creating the cluster
    
    Signed-off-by: Joe Talerico <rook@isovalent.com>

--- clouds/gke/kernel-swap/README.md
+++ clouds/gke/kernel-swap/README.md
@@ -5,3 +5,11 @@ To define the kernel we should swap to, just
 export KERNEL=<version>
 ```
 Then execute `./gke-kernel-tinker.sh` -- assumptions are that you have gcloud/gke setup already with creds :-)
+
+## Caveat 
+
+GKE has an `auto-repair` functionality, which will "repair" a node in the event the node is stuck in `NOT_READY` for > 10m.
+To avoid having `auto-repair` troll you, delete all pods that you created (meaning, go back to a clean cluster state). Cleaning
+will reduce the time it takes for all the pods to restart, and the node to get back into `READY`. Alternatively, see:
+
+https://cloud.google.com/kubernetes-engine/docs/how-to/node-auto-repair
--- clouds/gke/kernel-swap/gke-kernel-tinker.sh
+++ clouds/gke/kernel-swap/gke-kernel-tinker.sh
@@ -2,14 +2,25 @@
 set -ex
 region=us-central1-a
 name="${USER}-tinker"
-gcloud container clusters create "$name" \
-    --labels "usage=tinker,owner=${USER}" \
-    --num-nodes 2 \
-    --node-taints node.cilium.io/agent-not-ready=true:NoSchedule \
-    --image-type=UBUNTU_CONTAINERD \
-    --region $region 
-cilium install
-kubectl create -f cm-kernel.yaml
+bootstrap=${BOOTSTRAP:-false}
+cleanup=${CLEAN:-true}
+
+if $cleanup; then
+    cilium uninstall
+fi
+
+if $bootstrap; then
+    gcloud container clusters create "$name" \
+        --labels "usage=tinker,owner=${USER}" \
+        --num-nodes 2 \
+        --node-taints node.cilium.io/agent-not-ready=true:NoSchedule \
+        --image-type=UBUNTU_CONTAINERD \
+        --region $region 
+    cilium install
+    helm install prometheus prometheus-community/prometheus --namespace prometheus  --namespace prometheus --create-namespace
+fi
+
+kubectl apply -f cm-kernel.yaml
 envsubst < ds-kernel.yaml
 export KERNEL_VERSION=${KERNEL:-v5.4}
 envsubst < ds-kernel.yaml | kubectl apply -f -
@@ -17,5 +28,6 @@ kubectl rollout status ds/node-initializer
 for node in $(kubectl get nodes -o=custom-columns=:.metadata.name | grep -v -e '^[[:space:]]*$'); do
     gcloud compute instances reset --zone $region $node
 done
-sleep 10 # Wait for k8s to catchup to the reboot 
+kubectl delete -f ds-kernel.yaml
+sleep 30 # Wait for k8s to catchup to the reboot 
 kubectl wait --for=condition=Ready nodes --all --timeout=600s 
