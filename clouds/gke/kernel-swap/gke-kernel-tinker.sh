e025dc707bc8210793eac532608e604c8aff2f0f
Joe Talerico <rook@isovalent.com>
2022-04-11 09:25:25 -0400

    Allow user override of type and num nodes
    Allow the user to set the VM type and update the number of nodes to
    deploy
    
    Signed-off-by: Joe Talerico <rook@isovalent.com>

--- clouds/gke/kernel-swap/gke-kernel-tinker.sh
+++ clouds/gke/kernel-swap/gke-kernel-tinker.sh
@@ -4,6 +4,8 @@ region=us-central1-a
 name="${USER}-tinker"
 bootstrap=${BOOTSTRAP:-false}
 cleanup=${CLEAN:-true}
+num=${NODES:-2}
+itype=${TYPE:-e2-medium}
 
 if $cleanup; then
     cilium uninstall
@@ -12,12 +14,14 @@ fi
 if $bootstrap; then
     gcloud container clusters create "$name" \
         --labels "usage=tinker,owner=${USER}" \
-        --num-nodes 2 \
+        --num-nodes $num \
+        --machine-type $itype \
         --node-taints node.cilium.io/agent-not-ready=true:NoSchedule \
         --image-type=UBUNTU_CONTAINERD \
         --region $region 
-    cilium install
-    helm install prometheus prometheus-community/prometheus --namespace prometheus  --namespace prometheus --create-namespace
+    #cilium install
+    cilium install --version -service-mesh:v1.11.0-beta.1 --config enable-envoy-config=true --kube-proxy-replacement=probe
+
 fi
 
 kubectl apply -f cm-kernel.yaml
@@ -31,3 +35,7 @@ done
 kubectl delete -f ds-kernel.yaml
 sleep 30 # Wait for k8s to catchup to the reboot 
 kubectl wait --for=condition=Ready nodes --all --timeout=600s 
+# Install usual tooling
+if $bootstrap; then
+  helm install prometheus prometheus-community/prometheus --namespace prometheus  --namespace prometheus --create-namespace
+fi
